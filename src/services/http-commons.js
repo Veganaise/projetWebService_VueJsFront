bf478c67f9f908803d3cd2fde58d26f344be432b
Antoine <antoine.ganne@hotmail.fr>
2019-11-07 20:19:23 +0100

    Update http-commons.js

--- src/services/http-commons.js
+++ src/services/http-commons.js
@@ -5,10 +5,5 @@ export const HTTP = axios.create({
     baseURL: `http://localhost:8080/`,
     headers: {
         //Authorization: 'Bearer {token}'
-    },
-    paramsSerializer: function (params) {
-        // eslint-disable-next-line no-console
-        console.log("yooooooooooooooo"+Qs.stringify(params))
-        return Qs.stringify(params)
     }
 });
