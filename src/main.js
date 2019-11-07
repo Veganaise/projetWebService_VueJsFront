e06e919f7a5b6c94cec43d990a1419cb871b159a
Antoine <antoine.ganne@hotmail.fr>
2019-11-07 09:26:29 +0100

    :baby_chick:  npm install bootstrap vue

--- src/main.js
+++ src/main.js
@@ -4,8 +4,15 @@ import Vuex from 'vuex'
 import App from './App.vue'
 import {getters,state,actions,mutations} from "./movieStore";
 
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './custom.scss'
+
 Vue.config.productionTip = false; // <- utile? créé automatiquement par webstorm
 Vue.use(Vuex);
+Vue.use(BootstrapVue)
+
 
 new Vue({
   store:new Vuex.Store({state,getters,actions,mutations}), //instancie store
