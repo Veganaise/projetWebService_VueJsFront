04671157c711a83e5c8cdd77562609a139d78ee1
AliciaRocchia <alicia.rocchia@orange.fr>
2019-11-07 09:56:52 +0100

    Add bootstrap

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import {getters,state,actions,mutations} from "./movieStore";
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import './custom.scss'
 
 Vue.config.productionTip = false; // <- utile? créé automatiquement par webstorm
 Vue.use(Vuex);
